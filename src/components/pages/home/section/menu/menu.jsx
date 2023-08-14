5ae822c195aa12cd08c1bc37238b53efe5da86b1
Raja <s.rajarathinam1999@gmail.com>
2023-08-14 23:16:28 +0530

    init responsive

--- src/components/pages/home/section/menu/menu.jsx
+++ src/components/pages/home/section/menu/menu.jsx
@@ -1,18 +1,18 @@
 const { Row, Col, Image, Container } = require("react-bootstrap");
+import MENU from "@/data/menu";
 import styles from "./menu.module.scss";
 
 const Menu = () => {
   return (
     <Container>
       <Row className={styles.menu}>
-        <Col lg={6} className={styles.col}>
-          <div>
-            <Image src="/images/menu/menu1.jpg" alt="menu1" fluid />
-          </div>
-        </Col>
-        <Col lg={6} className={styles.col}>
-          <Image src="/images/menu/menu2.jpg" alt="menu2" fluid />
-        </Col>
+        {MENU.map((m) => (
+          <Col lg={6} className={styles.col} key={m.id}>
+            <div>
+              <Image src={`/images/menu/${m.src}.jpg`} alt="menu1" fluid />
+            </div>
+          </Col>
+        ))}
       </Row>
     </Container>
   );
---
+++ src/data/menu.js
@@ -0,0 +1,12 @@
+const MENU = [
+    {
+        id:'menu1',
+        src:'menu1'
+    },
+    {
+        id:'menu2',
+        src:'menu2'
+    },
+]
+
+export default MENU
